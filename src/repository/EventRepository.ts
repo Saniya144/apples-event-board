919c7ff19ac3e3154fc51274bf257ceef02ed06a
sammitnepal <nepalsammit@gmail.com>
2026-04-12 17:11:19 -0400

    feat: added repo methods and errors

---
+++ src/repository/EventRepository.ts
@@ -0,0 +1,9 @@
+import { Result } from "../lib/result";
+import { IEvent } from "../model/Event";
+import { EventError } from "../event/errors";
+
+export interface IEventRepository{
+    create(event: IEvent): Promise<Result<IEvent,EventError>>
+    findbyId(id: string): Promise<Result<IEvent | null, EventError>> ;
+    update(event: IEvent): Promise<Result<IEvent,EventError>>
+}

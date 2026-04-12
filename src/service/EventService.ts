47611f1ff363fb28eab8a24cb6eb35b05985c6d9
sammitnepal <nepalsammit@gmail.com>
2026-04-12 18:03:13 -0400

    feat: added service method

---
+++ src/service/EventService.ts
@@ -0,0 +1,31 @@
+import { UnexpectedDependencyError } from "../event/errors";
+import { EventError } from "../event/errors";
+import { Result ,Ok,Err} from "../lib/result";
+import { IEvent } from "../model/Event";
+import { IEventRepository } from "../repository/EventRepository";
+
+
+export class EventService{
+    constructor(private readonly repo: IEventRepository){}
+    async createEvent(input: any,user:any): Promise<Result<IEvent,EventError>>{
+        if(!input.title|| input.title.trim()==="") return Err(UnexpectedDependencyError("title is required"))
+        if(!input.location|| input.location.trim()==="") return Err(UnexpectedDependencyError("location is required"))
+        const begin =  new Date(input.startTime);
+        const end = new Date(input.endTime);
+        if(end<begin) return Err(UnexpectedDependencyError("invalid end is before start"))
+        const event: IEvent={
+            id: Math.random().toString(),
+            title: input.title,
+            description: input.description||"",
+            location: input.location,
+            startTime: begin,
+            endTime: end,
+            organzierID: user.id,
+            status: "draft",
+        };
+        return await this.repo.create(event);
+
+
+
+    }
+}

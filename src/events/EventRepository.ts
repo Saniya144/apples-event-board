2130c7937bd6f2b5df53d201f9205b338208a2a3
Saniya144 <saniya.jain144@gmail.com>
2026-04-15 17:07:54 -0400

    refactor: fix composition wiring and remove duplicate event structure

--- src/events/EventRepository.ts
+++
@@ -1,9 +0,0 @@
-import type { Result } from "../lib/result";
-import type { IEvent } from "./Event";
-
-export interface IEventRepository {
-  findById(eventId: string): Promise<Result<IEvent | null, Error>>;
-  create(event: IEvent): Promise<Result<IEvent, Error>>;
-  update(event: IEvent): Promise<Result<IEvent, Error>>;
-  list(): Promise<Result<IEvent[], Error>>;
-}
